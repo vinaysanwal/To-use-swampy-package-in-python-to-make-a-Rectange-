efc013d575320bb763ddf7ce02b77fe7a0f1a981
Vinay <sanwalvinay@gmail.com>
2017-08-23 10:12:24 +0530

    First Commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import Tkinter
+import swampy
+from swampy.TurtleWorld import * 
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+for i in range(4):
+	fd(bob , 100)
+	lt(bob)
+
+wait_for_user()
